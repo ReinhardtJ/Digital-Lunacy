091bd39bfc9a47ae5007cbcc11c47e3157c3f171
Jonas Reinhardt <ersatz.jonas@yahoo.de>
2018-07-18 19:29:46 +0200

    Added Autism

---
+++ Digital Lunacy/Assets/Audio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+
+    public AudioSource audioData;
+	// Use this for initialization
+	void Start () {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(!audioData.isPlaying)
+        {
+            audioData.Play(0);
+        }
+	}
+}
---
+++ Digital Lunacy/Assets/CamaraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour {
+    GameObject player;
+    GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        camera = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camera.transform.position= new Vector3(player.transform.position.x, 
+            player.transform.position.y, camera.transform.position.z);
+
+	}
+}
