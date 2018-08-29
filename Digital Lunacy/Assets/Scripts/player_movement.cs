41733da26c363b865d9fd8a494c41187865d98d1
Jonas Reinhardt <ersatz.jonas@yahoo.de>
2018-08-30 00:02:47 +0200

    added jumping & improved player movement script

--- Digital Lunacy/Assets/Scripts/player_movement.cs
+++ Digital Lunacy/Assets/Scripts/player_movement.cs
@@ -1,26 +1,69 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class player_movement : MonoBehaviour {
 
+    // the player object this script is attached to 
     public GameObject player;
+    // as far the player can move per frame
     public float verticalMovementFactor;
+    // as much force the player recieves with each jump
+    public float jumpFactor;
+    //as many seconds have to pass until the player can jump again
+    public int maxJumpThreshold;
+
+    // counter variable from maxJumpThreshold to 0
+    private int jumpThreshold;
 
 	void Start () {
-		
-	}
-	
+        jumpThreshold = 0;
 
-	void Update () {
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    void FixedUpdate () {
+
         Vector3 currentPos = player.transform.position;
-        if (Input.GetKey("left")){
-            player.transform.Translate(-verticalMovementFactor, 0, 0);
-        } else if (Input.GetKey("right"))
+        if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            player.transform.Translate(verticalMovementFactor, 0, 0);
-
+            moveLeft();
+        } else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveRight();
+        } else { // when the player doesn't move left or right 
         }
+
+        if((Input.GetKey("up") || Input.GetKey("w")) && jumpThreshold == 0)
+        {
+            if (player.GetComponent<PolygonCollider2D>().IsTouchingLayers())
+            {
+                jump();
+                jumpThreshold = maxJumpThreshold;
+            }
+        }
+        jumpThreshold = jumpThreshold == 0 ? 0 : jumpThreshold - 1; 
+    }
+
+    private void jump()
+    {
+        player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpFactor));
+    }
+
+    private void moveLeft()
+    {
+        player.transform.Translate(-verticalMovementFactor, 0, 0);
+    }
+
+    private void moveRight()
+    {
+        player.transform.Translate(verticalMovementFactor, 0, 0);
 
     }
 }
